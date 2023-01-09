0a660dfd73eed93ffed2d2424fba154220537ca4
Beninoonet <beninoonet@pm.me>
2023-01-09 17:54:09 +0100

    finish userinfo context menu

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,6 +1,6 @@
 const { Command } = require('@sapphire/framework');
 const { ApplicationCommandType } = require('discord-api-types/v9');
-const { GuildMember , Interaction } = require('discord.js');
+const { GuildMember , Interaction, MessageEmbed } = require('discord.js');
 class SlashCommand extends Command {
   constructor(context, options) {
     super(context, {
@@ -20,10 +20,22 @@ class SlashCommand extends Command {
    * @param {Interaction} interaction 
    */
   async contextMenuRun(interaction) {
-    // Use isUserContextMenu() to ensure this command was executed as a User Context Menu Command
     if (interaction.isUserContextMenu() && interaction.targetMember instanceof GuildMember) {
+      const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setThumbnail(interaction.targetMember.user.displayAvatarURL({ dynamic: true }))
+      .setTitle(`Profil de ${interaction.targetMember.user.username}`)
+      .addFields(
+        {name: `Mention`, value: `${interaction.targetMember.user}`, inline: true},
+        {name: `ID`, value: `${interaction.targetMember.user.id}`, inline: true},
+        {name: `Rôles`, value: `${interaction.targetMember.roles.cache.map(r => r).join(' ').replace('@everyone', " ") || "None"}`},
+        {name: `A rejoint le serveur le`, value: `<t:${parseInt(interaction.targetMember.joinedTimestamp / 1000)}:R>`, inline: true},
+        {name: `Compte créé le`, value: `<t:${parseInt(interaction.targetMember.user.createdTimestamp / 1000)}:R>`, inline: true},
+      )
+      .setTimestamp()
       await interaction.reply({
-        content: `${interaction.targetMember.user}`
+        embeds: [embed],
+        ephemeral: true
       })
     }
   }
