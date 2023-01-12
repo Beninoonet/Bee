58208bb7fa208ef8999c07c66a1f35263e43a409
Beninoonet <beninoonet@pm.me>
2023-01-12 12:52:33 +0100

    <3

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const { SapphireClient } = require('@sapphire/framework');
 require('dotenv').config
 
 const { TOKEN, DEV_TOKEN } = require('./config.json');
-const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'], });
 client.login(DEV_TOKEN);
